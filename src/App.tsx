246abcc51c95952a239cfa83c33a4fd9324eb7f6
hariirawan <hari31121993.com>
2018-05-08 07:10:49 +0700

    efect animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
+import * as uuid from 'uuid';
+import './App.css';
+
 import { Button, Grid, ListGroup, ListGroupItem } from 'react-bootstrap';
 
-import * as uuid from 'uuid';
+import {
+  CSSTransition, TransitionGroup,
+} from 'react-transition-group'
 
 interface IState {
   items: any;
@@ -13,36 +18,76 @@ class App extends React.Component<{}, IState> {
     this.state = {
       items: [
         {
+          address: 'Lombok Timur',
           id: uuid(),
-          text: 'Hari Irawan'
+          name: 'Hari Irawan',
         },
         {
+          address:'Bima',
           id: uuid(),
-          text: 'Julhan Arif'
+          name: 'Julhan Arif',
         },
         {
+          address:'Jogjakarta',
           id: uuid(),
-          text: 'Muhammad Edo'
+          name: 'Muhammad Edo',
         }
       ]
+    }
+    this.addItem = this.addItem.bind(this);
+  }
+
+  public addItem() {
+    const name = prompt("Enter som nama...");
+    const address = prompt("Enter some address...");
+    if(name || address) {
+      this.setState(state => ({
+        items:[
+          ...state.items,
+          {
+            address,
+            id:uuid(),
+            name,
+          }
+        ]
+      }))
     }
   }
   public render() {
     return (
       <Grid>
         <ListGroup>
+          <TransitionGroup>
           {
-            this.state.items.map(({ id, text }: any, key: number) => {
+            this.state.items.map(({ id, name, address }: any) => {
               return (
-                <ListGroupItem key={key}>
-                  <Button bsStyle="danger">
-                    X
-                  </Button>
-                  {text}
-                </ListGroupItem>
+                <CSSTransition 
+                  key={id}
+                  timeout={1000}
+                  classNames="fade"
+                  >
+                  <ListGroupItem 
+                    className="table">
+                    <Button bsStyle="danger">
+                      &times;
+                    </Button>
+                    <div className="col">
+                      {name}
+                    </div>
+                    <div className="col">
+                      {address}
+                    </div>
+                  </ListGroupItem>
+                </CSSTransition>
               )
             })
           }
+          </TransitionGroup>
+          <Button 
+            bsStyle="primary"
+            onClick={this.addItem}>
+            Add Item
+          </Button>
         </ListGroup>
       </Grid>
     );
